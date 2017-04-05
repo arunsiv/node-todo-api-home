b2cdabcb671adb3b0f6dbc2d2d4ee9fb561dbe06
Arun <Arun>
2017-04-05 19:44:42 -0400

    added comments

--- playground/mongodb-find.js
+++ playground/mongodb-find.js
@@ -6,6 +6,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     }
     console.log('Connected to MongoDB...');
 
+    //*** find
     db.collection('Todos').find({
         _id: new ObjectID('58e50654d2786527f8d75e08')
     }).toArray().then((docs) => {
@@ -15,7 +16,7 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
         console.log('Unable to fetch Todos', err);
     });
 
-    //get count
+    //*** get count
     db.collection('Todos').count().then((docs) => {
         console.log(`Todos Count: ${docs}`);
     }, (err) => {
